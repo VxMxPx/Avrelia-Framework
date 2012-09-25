6836ccfc28a6152ff5e79a4b134be93552eb5c37
Marko Gajst <m@avrelia.com>
2012-09-25 20:41:07 +0100

    Application controller and model cleaned.

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -2,32 +2,17 @@
 
 class Home_Controller
 {
-    /**
-     * Default Action
-     * --
-     * @return  void
-     */
+    # Default Action
     public function index()
     {
-        # Add jQuery
-        JQuery::Add();
-
-        # Set variable
-        View::assign(
-            'greeting', 
-            '<span class="fade">Hello from</span> Avrelia Framework');
-
         # Get Master template
         View::get('master')->as_master();
 
         # Get master's region
         View::get('home')->as_region('main');
     }
-    //-
 
-    /**
-     * For Ajax Request...
-     */
+    # For Ajax Request...
     public function greeting()
     {
         $Model = new Home_Model();
@@ -36,15 +21,10 @@ class Home_Controller
             'data' => $Model->sayHello(),
         ));
     }
-    //-
 
-    /**
-     * Not found!
-     */
+    # Not found!
     public function not_found_404()
     {
         Http::status_404_not_found('<h1>404: Not found!</h1>');
     }
-    //-
 }
-//--
